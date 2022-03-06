a647ee60775d0373b557c8faa54afba9641b2b64
Ryan Spears <sperj001@hotmail.com>
2022-03-06 20:24:17 +1030

    Renamed Tab1 to Fuel

---
+++ src/app/fuel/fuel.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel',
+  templateUrl: 'fuel.page.html',
+  styleUrls: ['fuel.page.scss']
+})
+export class FuelPage {
+
+  constructor() {}
+
+}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'fuel',
+        loadChildren: () => import('../fuel/fuel.module').then(m => m.FuelPageModule)
       },
       {
         path: 'tab2',
